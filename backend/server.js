ed54077c30af8d7ebadf5784a652965bdccc770c
Adamy <admye@ucdavis.edu>
2025-01-13 21:12:11 -0800

    POST api

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,34 @@
 import express from 'express';
 import dotenv from "dotenv";
 dotenv.config();
+import { connectDB } from './config/db.js';
+import Product from './model/product.model.js';
 
 const app = express();
 
-app.get("/products",(req,res) => {      //request & response
-    // res.send("Server is ready");
-})
+app.use(express.json()); // make it accept JSON data in the req.body
+
+app.post("/api/products", async (req,res) => {      //request & response
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({ success: false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct});
+    } catch (error) {
+        console.error("Error in Create product:", error.message);
+        res.status(500).json({ success: false, message: "Sever Error"});
+    }
+}); //postman for backend testing
+
+// console.log(process.env.MONGO_URI);   //testing mongo access method
 
 app.listen(5010,()=>{
+    connectDB();
     console.log("Server started at http://localhost:5010");
 })
