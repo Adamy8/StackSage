0149fef58985c01955ffb5c8c413473628b9cb71
Adamy <admye@ucdavis.edu>
2025-01-13 23:28:01 -0800

    PUT backpoint

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from './config/db.js';
 import Product from './model/product.model.js';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.json()); // make it accept JSON data in the req.body
@@ -14,7 +15,7 @@ app.get("/api/products",async (req,res) => {
         res.status(200).json({success:true , data:products});
     } catch (error) {
         console.log("Error in fetching products:", error.message);  //for debug
-        res.status(500).json({ success:false, message:"Server error"});
+        res.status(500).json({ success:false, message:"Server Error"});
     }
 });
 
@@ -32,9 +33,26 @@ app.post("/api/products", async (req,res) => {      //request & response
         res.status(201).json({ success: true, data: newProduct});
     } catch (error) {
         console.error("Error in Create product:", error.message);  //for debug
-        res.status(500).json({ success: false, message: "Sever Error"});
+        res.status(500).json({ success: false, message: "Server Error"});
     }
 }); //postman for backend testing
+
+app.put("/api/products/:id", async (req,res) => {       // app.patch is for updating some field, put for all field
+    const {id} = req.params;
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){   // make sure the id is found
+        return res.status(404).json({success:false, message:"Invalid Product Id"});
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({success:true, data:updatedProduct});
+    } catch (error) {
+        console.log("Error in updating products:", error.message); // for debug
+        res.status(500).json({success:false, message:"Server Error"});
+    }
+});
 
 app.delete("/api/products/:id/", async (req,res) => {
     const {id} = req.params;
