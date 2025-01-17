f5e25e1bca5236808f606a5942c9734679feb6b0
Adamy <admye@ucdavis.edu>
2025-01-17 01:06:48 -0800

    debug, version difference for chakra

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,36 @@
+import { Container, Flex, Text, Link, HStack, Button } from "@chakra-ui/react"
+import { FaRegSquarePlus } from "react-icons/fa6";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Container maxW={"1140px"} px={4}>
+        <Flex 
+            h={16} 
+            alignItems={"center"} 
+            justifyContent={"space-between"}
+            flexDir={{
+                base:"column",
+                sm:"row"
+            }}
+        >
+            <Text
+                fontSize={{base:"22", sm:"28"}}
+                fontWeight={"bold"}
+                textAlign={"center"}
+            >
+                <Link href={"/"}> Product store 🛒</Link>
+            </Text>
+            <HStack spacing={2} alignItems={"center"}>
+                <Link href={"/create"}>
+                    <Button>
+                        <FaRegSquarePlus />
+                    </Button>
+                </Link>
+            </HStack>
 
-export default Navbar
+        </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
