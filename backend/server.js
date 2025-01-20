d4877cda2dc957b632afd3e5edc8e47b9318c786
Adamy <admye@ucdavis.edu>
2025-01-20 02:58:49 -0800

    deploy api & ends connection

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,20 @@ import { connectDB } from './config/db.js';
 // import Product from './model/product.model.js';
 // import mongoose from 'mongoose';
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5010;      // || 5010 is for when forget to set PORT in .env
 app.use(express.json()); // make it accept JSON data in the req.body
-
 app.use("/api/products/",productRoutes);
+
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT,()=>{
     connectDB();
